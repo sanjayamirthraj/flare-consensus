f3762ae0477b968c9fe81db704160acca42a845b
sanjayamirthraj <sanjay.amirthraj@gmail.com>
2025-03-07 18:36:18 -0800

    added the scripts

--- run.sh
+++ run.sh
@@ -2,4 +2,6 @@
 
 # Run flare-ai-consensus Docker container with environment variables
 echo "Starting flare-ai-consensus container..."
+#add this line if you want to build the image
+#docker build -t flare-ai-consensus .
 docker run -p 80:80 -it --env-file .env flare-ai-consensus 
